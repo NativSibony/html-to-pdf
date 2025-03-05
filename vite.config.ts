6596ae50133b1002e4e5efc735d918495253e8be
NativSibony <nativsibony@gmail.com>
2025-03-05 19:03:53 +0200

    fixed lib mode

--- vite.config.ts
+++ vite.config.ts
@@ -3,24 +3,26 @@ import { resolve } from 'path';
 import dts from 'vite-plugin-dts';
 
 export default defineConfig({
+  base: './',
   build: {
+    sourcemap: true,
     lib: {
-      entry: resolve(__dirname, './src/index.ts'),
-      name: 'HTML PDF Converter',
-      fileName: 'html-to-pdfx',
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'HTML to PDF Converter',
+      formats: ['es', 'cjs', 'umd'],
+      fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
+      external: ['html2canvas', 'jsPDF'],
       output: {
+        extend: true,
         globals: {
           html2canvas: 'html2canvas',
           jsPDF: 'jsPDF',
         },
       },
     },
-    minify: true,
-    emptyOutDir: true,
-    outDir: 'dist',
   },
-  plugins: [dts()],
+  plugins: [dts({ rollupTypes: true })],
   worker: { format: 'es' },
 });
